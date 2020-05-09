afebf04210c97900ec75a35e1eda5f94ee7290f0
Jim <303683086@qq.com>
2020-05-09 17:23:02 +0800

    Fixing script

--- Gbook/_book/script/ImgHover.js
+++ Gbook/_book/script/ImgHover.js
@@ -10,10 +10,26 @@ window.onload=function(){
 
 function showImg(src)
 {
+	var posX = 0, posY = 0;
+
+	var event = event || window.event;
+
+	if (event.pageX || event.pageY)
+	{
+		posX = event.pageX;
+		posY = event.pageY;
+
+	} 
+	else if (event.clientX || event.clientY)
+	{
+		posX = event.clientX + document.documentElement.scrollLeft + document.body.scrollLeft;
+		posY = event.clientY + document.documentElement.scrollTop + document.body.scrollTop;
+	}
+
 	var IMGContainer = document.getElementById("ImgContainerID");
 	
-	IMGContainer.style.left 	= event.clientX;
-	IMGContainer.style.top 		= event.clientY;
+	IMGContainer.style.left 	= posX;
+	IMGContainer.style.top 		= posY; 
 	IMGContainer.innerHTML 		= "<img src=\"" + src + "\" />";
 	IMGContainer.style.display  = "block";
 }
